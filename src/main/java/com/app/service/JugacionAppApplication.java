4f873fc130290628aea7eac01ef460022db994d1
Metaphys1999 <alejandroaguadomarin1999@gmail.com>
2023-12-23 00:17:38 -0500

    Inicialización Proyecto Jugación Backend

---
+++ src/main/java/com/app/service/JugacionAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JugacionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JugacionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/service/JugacionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JugacionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
